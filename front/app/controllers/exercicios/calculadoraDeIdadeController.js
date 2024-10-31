ffe2da2900df14ccfa1316eb8b50697e552804b2
Marcos Rodrigues <mrodrigues.mm83@gmail.com>
2024-10-31 11:59:48 -0300

    calcular idade

--- front/app/controllers/exercicios/calculadoraDeIdadeController.js
+++ front/app/controllers/exercicios/calculadoraDeIdadeController.js
@@ -7,8 +7,22 @@ $scope.info = {
 };
 
 $scope.calcularIdade = function() {
+    DataDeNascimento = new Date($scope.info.DataDeNascimento);
+    hoje = new Date();
 
+    anos = hoje.getFullYear() - dataDeNascimento.getFullYear();
+    meses = hoje.getMonth() - dataDeNascimento.getMonth();
+    dias = hoje.getDate() - dataDeNascimento.getDate();
 
+    if (dias < 0) {
+        meses --;
+        dias += new Date(hoje.getFullYear(), hoje.getMonth(), 0).getDate();
+    }
+
+    if(meses < 0) {
+        anos --;
+        meses += 12;    
+    }
 
 
 };
